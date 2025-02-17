8130c5221dd861062ffe51e3d4369a6f6919f114
LIKHITHREDDYADDELA <laddela@albany.edu>
2025-02-17 01:49:36 -0500

    added chart.js and afterviewinit

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,23 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef,AfterViewInit } from '@angular/core';
+import {Chart, registerables} from 'chart.js';
+
 
 @Component({
   selector: 'app-product',
-  standalone: true,
-  imports: [],
+  standalone: false,
+
   template: `
     <p>
      U R CURRENTLY IN PRODUCTS PAGE !
     </p>
+    <canvas #chartCanvas></canvas>
+
   `,
-  styles: `
-  p{
-    color: Tomato;
-    font-size : 50px;
-      font-style :italic;
-      font-weight: bold;
-  }
-  `
+  styleUrl: `./product.component.scss`
 })
-export class ProductComponent {
+
+export class ProductComponent implements AfterViewInit{
+  @ViewChild('chartCanvas') chartElement!: ElementRef;
+
+  ngAfterViewInit(): void {
+    Chart.register(...registerables); // Register required modules
+
+    new Chart(this.chartElement.nativeElement, {
+      type: 'line', // Change to 'line', 'pie', 'doughnut' if needed
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        datasets: [{
+          label: 'Sales',
+          data: [40, 55, 75, 90, 120],
+          backgroundColor: ['red', 'blue', 'green', 'purple', 'orange']
+        }]
+      },
+      options: {
+        responsive: true
+      }
+    });
+  }
 
 }
